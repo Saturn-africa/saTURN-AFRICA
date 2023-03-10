025d485f00e36692b4408e27f2b24823ff6adc07
jonyoung123 <stephenjohn5646@gmail.com>
2023-03-10 12:11:05 +0100

    feat/user_account; enabled select image on profile screen;

--- lib/customer_info/find_roommates/account/account_screen.dart
+++ lib/customer_info/find_roommates/account/account_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 import 'package:saturn/custom_widgets/custom_profile_tile.dart';
+import 'package:saturn/custom_widgets/image_picker.dart';
 import 'package:saturn/customer_info/find_roommates/life_style.dart';
 import 'package:saturn/customer_info/find_roommates/personal_info.dart';
 import 'package:saturn/customer_info/find_roommates/room_owner/additional_info.dart';
@@ -13,22 +17,31 @@ import 'package:saturn/helper_widgets/colors.dart';
 import 'package:saturn/helper_widgets/text_style.dart';
 import 'package:saturn/providers/customer_info_provider.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
   const Profile({this.isDrawer = false, super.key});
   final bool isDrawer;
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  XFile? _pickedImg;
+  final ImagePicker _picker = ImagePicker();
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     dynamic status =
         context.watch<CustomerInfoProvider>().customerInfo["status"] ?? "";
     return Scaffold(
-      appBar: isDrawer
+      appBar: widget.isDrawer
           ? AppBar(
               title: Text("Profile", style: appBarTextStyle),
               leading: IconButton(
                   icon: const Icon(
                     Icons.arrow_back,
-                    color: Colors.black,
+                    color: black,
                   ),
                   onPressed: () {
                     Navigator.pop(context, true);
@@ -48,16 +61,19 @@ class Profile extends StatelessWidget {
                 FittedBox(
                   child: Row(
                     children: [
-                      Container(
-                        width: 120,
-                        height: 120,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            image: const DecorationImage(
-                                scale: 0.7,
-                                image:
-                                    AssetImage('assets/images/download.png')),
-                            border: Border.all(color: purple, width: 2)),
+                      GestureDetector(
+                        onTap: () => showModal(),
+                        child: Container(
+                          width: 120,
+                          height: 120,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              image: const DecorationImage(
+                                  scale: 0.7,
+                                  image:
+                                      AssetImage('assets/images/download.png')),
+                              border: Border.all(color: purple, width: 2)),
+                        ),
                       ),
                       const SizedBox(width: 10),
                       Column(
@@ -66,17 +82,23 @@ class Profile extends StatelessWidget {
                         children: [
                           Row(
                             children: [
-                              Container(
-                                width: 30,
-                                height: 30,
-                                decoration: BoxDecoration(
-                                    color:
-                                        const Color.fromRGBO(243, 245, 251, 1),
-                                    borderRadius: const BorderRadius.all(
-                                        Radius.circular(60)),
-                                    border: Border.all(
-                                        color: const Color.fromRGBO(
-                                            51, 51, 51, 0.4))),
+                              GestureDetector(
+                                onTap: () {
+                                  if (_pickedImg != null) {
+                                    Navigator.of(context, rootNavigator: true)
+                                        .push(MaterialPageRoute(
+                                            builder: (_) => ShowImagePage(
+                                                pickedImage: _pickedImg!)));
+                                  }
+                                },
+                                child: CircleAvatar(
+                                  radius: 20,
+                                  backgroundColor:
+                                      const Color.fromRGBO(243, 245, 251, 1),
+                                  backgroundImage: _pickedImg == null
+                                      ? null
+                                      : FileImage(File(_pickedImg!.path)),
+                                ),
                               ),
                               Padding(
                                 padding: const EdgeInsets.all(8.0),
@@ -175,5 +197,82 @@ class Profile extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _pickImg(ImageSource src) async {
+    final pickedImgFile =
+        await _picker.pickImage(source: src, imageQuality: 50);
+    if (pickedImgFile != null) {
+      setState(() {
+        _pickedImg = XFile(pickedImgFile.path);
+      });
+    }
+  }
+
+  Future showModal() {
+    return showModalBottomSheet(
+        context: context,
+        builder: (context) => Container(
+              margin: const EdgeInsets.only(bottom: 50),
+              child: SelectOptions(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextButton.icon(
+                      onPressed: () {
+                        Navigator.pop(context);
+                        _pickImg(ImageSource.camera);
+                      },
+                      icon: Icon(Icons.photo_camera_outlined,
+                          color: Theme.of(context).primaryColor),
+                      label: const Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Add image from Camera',
+                          style: TextStyle(
+                            color: lightBlack,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                    TextButton.icon(
+                      onPressed: () {
+                        Navigator.pop(context);
+                        _pickImg(ImageSource.gallery);
+                      },
+                      icon: Icon(Icons.image_rounded,
+                          color: Theme.of(context).primaryColor),
+                      label: const Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Add image from Gallery',
+                          style: TextStyle(
+                            color: lightBlack,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ));
+  }
+}
+
+class ShowImagePage extends StatelessWidget {
+  const ShowImagePage({super.key, required this.pickedImage});
+  final XFile pickedImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Image(
+      image: FileImage(File(pickedImage.path)),
+      fit: BoxFit.cover,
+    ));
   }
 }
