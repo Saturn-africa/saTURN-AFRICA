c88c298c1c747203a8fdcfc6271175bb80f877e2
jonyoung123 <stephenjohn5646@gmail.com>
2023-03-22 18:14:45 +0100

    feat/homes_for_rent; refactored the home screen; changed the bottom navigation

--- lib/customer_info/homes_for_rent/home_page/navigation.dart
+++ lib/customer_info/homes_for_rent/home_page/navigation.dart
@@ -1,109 +1,73 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:saturn/customer_info/find_roommates/account/account_screen.dart';
-import 'package:saturn/customer_info/find_roommates/shortlisted/shortlisted_owner.dart';
-import 'package:saturn/customer_info/homes_for_rent/explore/explore_page.dart';
+import 'package:provider/provider.dart';
+import 'package:saturn/custom_widgets/drawer_widget.dart';
+import 'package:saturn/customer_info/homes_for_rent/filter_page.dart';
 import 'package:saturn/helper_widgets/colors.dart';
 import 'package:saturn/helper_widgets/text_style.dart';
+import 'package:saturn/providers/home_provider.dart';
+
+import '../../../helper_widgets/home/bottom_item.dart';
 
 class HomesNavigation extends StatelessWidget {
   const HomesNavigation({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(
-          activeColor: purple,
-          currentIndex: 1,
-          height: 60,
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-                icon: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      SvgPicture.asset("assets/icons/shortlisted.svg",
-                          height: 22, width: 25),
-                      Text("Shortlisted", style: bottomNavTextStyle)
-                    ]),
-                activeIcon: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      SvgPicture.asset("assets/icons/shortlisted.svg",
-                          height: 22, width: 25, color: purple),
-                      Text("Shortlisted", style: bottomNavTextStyle)
-                    ])),
-            BottomNavigationBarItem(
-              icon: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    SvgPicture.asset("assets/icons/explore.svg",
-                        height: 22, width: 25),
-                    Text("Explore", style: bottomNavTextStyle)
-                  ]),
-              activeIcon: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    SvgPicture.asset("assets/icons/explore.svg",
-                        color: purple, height: 22, width: 25),
-                    Text("Explore", style: bottomNavTextStyle)
-                  ]),
+    Size size = MediaQuery.of(context).size;
+    return Consumer<HomeProvider>(
+      builder: (_, home, __) => Scaffold(
+        appBar: AppBar(
+          leading: const DrawerWidget(),
+          centerTitle: true,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                'assets/icons/logo.svg',
+                color: purple,
+              ),
+              SizedBox(width: size.width * 0.05),
+              Text("Saturn", style: appBarTextStyle),
+            ],
+          ),
+          actions: [
+            IconButton(
+              iconSize: 40,
+              icon: SvgPicture.asset(
+                'assets/icons/filter.svg',
+                color: deepBlack,
+              ),
+              onPressed: () {
+                Navigator.of(context, rootNavigator: true).push(
+                    MaterialPageRoute(
+                        fullscreenDialog: true,
+                        builder: ((context) => const HomesFilterPage())));
+              },
             ),
-            BottomNavigationBarItem(
-              icon: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    SvgPicture.asset("assets/icons/account.svg",
-                        height: 22, width: 25),
-                    Text("Account", style: bottomNavTextStyle)
-                  ]),
-              activeIcon: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    SvgPicture.asset("assets/icons/account.svg",
-                        color: purple, height: 22, width: 25),
-                    Text("Account", style: bottomNavTextStyle)
-                  ]),
-            ),
-          ]),
-      backgroundColor: Colors.white,
-      tabBuilder: (context, index) {
-        switch (index) {
-          case 0:
-            return CupertinoTabView(
-              builder: (context) {
-                return const CupertinoPageScaffold(
-                  child: ShortlistedScreen(),
-                );
-              },
-            );
-          case 1:
-            return CupertinoTabView(
-              builder: (context) {
-                return const CupertinoPageScaffold(
-                  child: HomesExplore(),
-                );
-              },
-            );
-          case 2:
-            return CupertinoTabView(
-              builder: (context) {
-                return const CupertinoPageScaffold(
-                  child: Profile(),
-                );
-              },
-            );
-
-          default:
-            return CupertinoTabView(
-              builder: (context) {
-                return const CupertinoPageScaffold(
-                  child: HomesExplore(),
-                );
-              },
-            );
-        }
-      },
+          ],
+          backgroundColor: Colors.white,
+          elevation: 0.2,
+        ),
+        body: home.ownerScreens.elementAt(home.rentSelectedIndex),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: home.rentSelectedIndex,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          onTap: (index) => home.updateRentIndex(index),
+          elevation: 5,
+          items: bottomNavItem(),
+          selectedItemColor: black,
+          unselectedItemColor: black,
+          selectedIconTheme:
+              const IconThemeData(color: black, size: 23, weight: 2),
+          unselectedIconTheme:
+              const IconThemeData(color: white, size: 20, weight: 1),
+          selectedLabelStyle: bottomNavTextStyle,
+          unselectedLabelStyle: bottomNavTextStyle,
+        ),
+      ),
     );
   }
 }
--- lib/providers/home_provider.dart
+++ lib/providers/home_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:saturn/customer_info/find_roommates/room_owner/explore/explore_owner.dart';
 import 'package:saturn/customer_info/find_roommates/shortlisted/shortlisted_owner.dart';
+import 'package:saturn/customer_info/homes_for_rent/explore/explore_page.dart';
 
 import '../customer_info/find_roommates/account/account_screen.dart';
 import '../customer_info/find_roommates/room_seeker/explore/seeker_explore.dart';
@@ -17,7 +18,13 @@ class HomeProvider extends ChangeNotifier {
     const OwnerExplore(),
     const Profile()
   ];
-  List<Widget> rentScreens = [];
+
+  List<Widget> rentScreens = [
+    const ShortlistedScreen(),
+    const HomesExplore(),
+    const Profile()
+  ];
+
   int _selectedIndex = 1;
   int _ownerSelectedIndex = 1;
   int _rentSelectedIndex = 1;
